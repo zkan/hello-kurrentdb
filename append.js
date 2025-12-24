527c64956defec32203b029c62ed33d586e453ba
zkan <kan@odds.team>
2025-12-24 11:57:26 +0700

    Append a new event to stream

---
+++ append.js
@@ -0,0 +1,24 @@
+import { EventStoreDBClient, jsonEvent } from '@eventstore/db-client'
+
+const client = EventStoreDBClient.connectionString('esdb://0.0.0.0:2113?tls=false')
+
+// Create a new event
+const eventType = 'SampleEventType'
+const event = jsonEvent({
+  type: eventType,
+  data: {
+    'id': '1',
+    'importantData': 'some value',
+  },
+})
+
+// Append the event object into the stream
+const eventStream = 'SampleStream'
+await client.appendToStream(eventStream, event)
+
+console.log('Stream: ' + eventStream)
+console.log('Event Type: ' + eventType)
+console.log('Event Body: ' + JSON.stringify(event.data))
+console.log("************************");
+
+client.dispose();
